245dc610114b7628aec7940a4fdc5716fa39730c
Mohamed <benthameur.mohamedali@gmail.com>
2026-02-28 14:56:16 +0100

    Fix: wire EmojiProvider (twemoji) into AppProviders so emojis render

--- components/AppProviders.tsx
+++ components/AppProviders.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { LanguageProvider } from './LanguageProvider';
+import EmojiProvider from './EmojiProvider';
 
 export default function AppProviders({ children }: { children: React.ReactNode }) {
     return (
         <LanguageProvider>
+            <EmojiProvider />
             {children}
         </LanguageProvider>
     );
